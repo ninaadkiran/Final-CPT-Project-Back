cfb8823c12fb01a84d62b85b671415aebfa3b5d3
ninaadkiran <ninaadkiran@gmail.com>
2024-04-30 02:45:53 +0000

    code cleanup

--- main.py
+++ main.py
@@ -50,4 +50,4 @@ app.cli.add_command(custom_cli)
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing
-    app.run(debug=True, host="0.0.0.0", port="8088")
+    app.run(debug=True, host="0.0.0.0", port="8089")
